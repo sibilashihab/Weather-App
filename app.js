ae7ea70aa9b1af59738dbc5b1406792577cb03b1
Sibila <ss2190@hw.ac.uk>
2024-01-11 16:55:52 +0000

    refined code

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const weathericon = document.querySelector(".icon");
 
 async function checkWeather(city) {
      const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+
      if (response.status == 404) {
           document.querySelector(".error").style.display = "block";
           document.querySelector(".weather").style.display = "none";
      }
      else {
           var data = await response.json();
-          console.log(data);
 
           document.querySelector(".city").innerHTML = data.name;
           document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -46,6 +46,7 @@ async function checkWeather(city) {
           else if (data.weather[0].main == "Snow") {
                weathericon.src = "images/snow.png"
           }
+
           document.querySelector(".weather").style.display = "block";
           document.querySelector(".error").style.display = "none";
      }
